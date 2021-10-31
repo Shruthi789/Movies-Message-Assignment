cedf7268a72fe8b0a3a4c50b4a8271d7153dcbaf
Shruthi Sridhar <shruthisridhar96@gmail.com>
2021-10-31 10:49:01 +0530

    React-Assignment-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,183 @@
 import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+const data = [
+  {
+    name: "Shruthi",
+    img: "https://webstockreview.net/images/clipart-boy-phone-11.png"
+  },
+  {
+    name: "Radhika",
+    img:
+      "https://clipground.com/images/hindu-woman-clipart-5.png"
+  },
+  {
+    name: "Sridhar",
+    img:
+      "https://images.onlinelabels.com/images/clip-art/Juhele/Business%20man%20using%20a%20phone-284999.png"
+  }
+];
+const movies = [
+  {
+    name: "Interstellar",
+    poster:
+      "https://thefilmstage.com/wp-content/uploads/2014/09/interstellar_poster.jpg",
+    summary:
+      "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    rating: "8.6",
+    cast: [
+      "Matthew McConaughey",
+      "Anne Hathaway",
+      "Jessica Chastain",
+      "Bill Irwin",
+      "Ellen Burstyn",
+      "Michael Caine"
+    ]
+  },
+  {
+    name: "Alvin and the Chipmunks",
+    poster:
+      "https://fanart.tv/fanart/movies/55301/movieposter/alvin-and-the-chipmunks-chipwrecked-56b79ff0afb57.jpg",
+    summary:
+      "Three musical chipmunks are discovered by an aspiring songwriter who wants to use their amazing singing abilities to become famous.",
+    rating: "8.6",
+    cast: [
+      "Jason Lee",
+      "David Cross",
+      "Cameron Richardson",
+      "Justin Long",
+      "Matthew Gray Gubler",
+      "Jesse McCartney"
+    ]
+  },
+  {
+    name: "Avengers",
+    poster:
+      "https://www.themoviedb.org/t/p/original/pdhOE0NAZaPzjsgTvatRP1xFhG3.jpg",
+    summary:
+      "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+    rating: "8.0",
+    cast: [
+      "Robert Downey Jr.",
+      "Chris Evans",
+      "Mark Ruffalo",
+      "Chris Hemsworth",
+      "Scarlett Johansson",
+      "Jeremy Renner",
+      "Tom Hiddleston",
+      "Clark Gregg",
+      "Cobie Smulders",
+      "Stellan Skarsgård",
+      "Samuel L. Jackson"
+    ]
+  },
+  {
+    name: "Avatar",
+    poster:
+      "https://www.themoviedb.org/t/p/original/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg",
+    summary:
+      "A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+    rating: "7.8",
+    cast: [
+      "Sam Worthington",
+      "Zoe Saldana",
+      "Stephen Lang",
+      "Michelle Rodriguez",
+      "Sigourney Weaver"
+    ]
+  },
+  {
+    name: "Harry Potter & the Deathly Hallows",
+    poster:
+      "https://tonights.tv/wp-content/uploads/movies/poster/12445-poster.jpg",
+    summary:
+      "Harry, Ron, and Hermione search for Voldemort's remaining Horcruxes in their effort to destroy the Dark Lord as the final battle rages on at Hogwarts.",
+    rating: "8.1",
+    cast: [
+      "Daniel Radcliffe",
+      "Rupert Grint",
+      "Emma Watson",
+      "Helena Bonham Carter",
+      "Robbie Coltrane",
+      "Warwick Davis",
+      "Ralph Fiennes",
+      "Michael Gambon",
+      "John Hurt",
+      "Jason Isaacs",
+      "Gary Oldman",
+      "Alan Rickman",
+      "Maggie Smith",
+      "David Thewlis",
+      "Julie Walters"
+    ]
+  },
+  {
+    name: "Kanaa",
+    poster:
+      "https://www.weekendpopcorn.com/wp-content/uploads/2018/12/kanaa-posters-5-768x1024.jpg",
+    summary:
+      "A woman who hails from a middle-class family background aims to excel in the competitive world of cricket with the help of her supportive father.",
+    rating: "7.8",
+    cast: [
+      "Aishwarya Rajesh",
+      "Sathyaraj",
+      "Darshan",
+      "Sivakarthikeyan"
+    ]
+  }
+];
+function Msg({ name, img }) {
+  return (
+    <div className="msg-spec">
+       <h1> Hi {name}!</h1> &nbsp;
+        <img src={img} width="50" height="50" alt="PersonImage" />
+    </div>
+  );
+}
+function Movies({ name, poster, summary, rating, cast }) {
+  return (
+    <div className="movie-data">
+      <img src={poster} className="movie-poster" alt="MovieImage" /> <br />
+      <div className="movie-spec">
+      <h4 className="movie-name">{name}</h4>
+      <h4 className="movie-rating">⭐ {rating} </h4>
+      </div>
+      <p className="movie-summary">{summary} </p>
+      <p className="movie-cast"> <span className="title">Cast:</span>{cast.toString()} </p>
+      <Counter />
+    </div>
+  );
+}
+function Counter(){
+  const [like,setLike]=useState(0);
+  const [dislike,setdisLike]=useState(0);
+  return (<div className="button-style">
+    <button onClick={()=>setLike(like+1)}>👍 {like}</button>
+    <button onClick={()=>setdisLike(dislike+1)}>👎 {dislike}</button>
+  </div>)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <h1 className="heading">Hello Messages</h1>
+       <div className="msg-arrangement">
+      {data.map(({ name, img }) => (
+        <Msg name={name} img={img} />
+      ))}
+      </div>
+      <h1 className="heading">Movies Details</h1>
+      <div className="movies-arrangement">
+      {movies.map(({ name, poster, summary, rating, cast }) => (
+        <Movies
+          name={name}
+          poster={poster}
+          summary={summary}
+          rating={rating}
+          cast={cast}
+        />
+      ))}
+      </div>
     </div>
   );
 }
